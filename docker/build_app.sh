77868a3b918f941c01f1a15615fcd30d94c3e379
Siroshun09 <siro.shun8@gmail.com>
2025-12-04 06:14:32 +0900

    chore: move Go project under app directory

--- docker/build_app.sh
+++ docker/build_app.sh
@@ -11,12 +11,16 @@ fi
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_DIR" || exit 1
 
+cd ../app || exit 1
+
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 \
-    go build -o "./bin/${CMD_NAME}" \
+    go build -o "$SCRIPT_DIR/bin/${CMD_NAME}" \
     -ldflags="-s -w" \
     -trimpath \
     -buildvcs=false \
     -mod=readonly \
-    "../internal/cmd/${CMD_NAME}/main.go"
+    "./cmd/${CMD_NAME}/main.go"
+
+cd "$SCRIPT_DIR" || exit 1
 
 docker build -t "${IMAGE_NAME}:latest" --target "${CMD_NAME}" .
